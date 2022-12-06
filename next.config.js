ccd06495ee59afd5674ee007b83c721706926466
elliotfiske <elliotfiske@gmail.com>
2022-12-05 23:08:16 -0800

    More stuff that I think I need.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
     // Required:
     appDir: true,
+    assetPrefix: isProd ? '/your-github-repo-name/' : '',
+    images: {
+      unoptimized: true,
+    },
   },
   basePath: '/tb-santa-jam-2022',
 };
