7b26cf05dc33b7501959cae65d9bc1dde8e911ba
elliotfiske <elliotfiske@gmail.com>
2022-12-22 23:49:00 -0800

    maybe fix images

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production"
 
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  // images: {
-  //   unoptimized: true,
-  // },
-  assetPrefix: isProd ? '/tb-santa-jam-2022' : '',
-  basePath: isProd ? '/tb-santa-jam-2022' : '',
-};
+    reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+    assetPrefix: isProd ? "/tb-santa-jam-2022" : "",
+    basePath: isProd ? "/tb-santa-jam-2022" : "",
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
